9bb132520bf6ae2dc17b63ae98dc1dde4f5ea456
kahgold <52286387+kahgold@users.noreply.github.com>
2023-10-26 21:26:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,31 @@ import plotly.express as px
 import streamlit as stm
      
 df_cars = pd.read_csv('vehicles_us.csv') # lendo os dados
+
 hist_button = stm.button('Criar histograma') # criar um botão
-scat_button = stm.button('Criar gráfico de dispersão')
-mtx_corr_button = tsm.button('Criar matriz de correlação')
+scat_button = stm.button('Criar gráfico de dispersão') # criar um botão
+mtx_corr_button = stm.button('Criar matriz de correlação') # criar um botão
+box_buttom = stm.button('Criar boxplot') # criar um botão
+bar_button = stm.buttom('Criar gráfico de barra') # criar um botão
      
 if hist_button: # se o botão for clicado
      # escrever uma mensagem
      stm.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
 if scat_button: # se o botão for clicado
+     # escrever uma mensagem
      stm.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
 if mtx_corr_button: # se o botão for clicado
+     # escrever uma mensagem
      stm.write('Criando uma matriz de correlação para o conjunto de dados de anúncios de vendas de carros')
 
 if box_button: # se o botão for clicado
+     # escrever uma mensagem
+     stm.write('Criando uma matriz de correlação para o conjunto de dados de anúncios de vendas de carros')
+
+if bar_button: # se o botão for clicado
+     # escrever uma mensagem
      stm.write('Criando uma matriz de correlação para o conjunto de dados de anúncios de vendas de carros')
          
      # criar um histograma
@@ -29,28 +39,37 @@ if box_button: # se o botão for clicado
      # criar uma matriz de correlação
      fig3 = px.imshow(df_cars, df_cars.iloc[:, [0, 1, 4, 6, 10]].corr())
 
-     # criar uma matriz de correlação
+     # criar um boxplot
      fig4 = px.box(df_cars, x='paint_color', y='price', color='paint_color')
+
+     # criar um gráfico de barra
+     fig5 = px.box(df_cars, x="paint_color", template='none', color='paint_color')
          
      # exibir um gráfico Plotly interativo
      stm.plotly_chart(fig1, use_container_width=True)
      stm.plotly_chart(fig2, use_container_width=True)
      stm.plotly_chart(fig3, use_container_width=True)
      stm.plotly_chart(fig4, use_container_width=True)
+     stm.plotly_chart(fig5, use_container_width=True)
 
 # criar uma caixa de seleção
 build_histogram = stm.checkbox('Criar um histograma')
 build_scatter = stm.checkbox('Criar um gráfico de dispersão)
 build_mtx_corr = stm.checkbox('Criar uma matriz de correlação')
 build_box = stm.checkbox('Criar um boxplot')
+build_bar = stm.checkbox('Criar um boxplot')
 
 if build_histogram: # se a caixa de seleção for selecionada
      stm.write('Criando um histograma para a coluna odometer')
 
 if build_scatter: # se a caixa de seleção for selecionada
-    stm.write('Criando um gráfico de dispersão para as colunas model_year e odometer')
+     stm.write('Criando um gráfico de dispersão para as colunas model_year e odometer')
 
 if build_mtx_corr: # se a caixa de seleção for selecionada
-    stm.write('Criando uma matriz de correlação para o dataframe df_cars')
+     stm.write('Criando uma matriz de correlação para o dataframe df_cars')
 
-if 
+if build_box:
+     stm.write('Criando um boxplot para as colunas paint_color e price')
+
+if build_bar:
+     stm.write('Criando um gráfico de barra para a coluna price')
